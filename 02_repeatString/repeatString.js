cfc16b3411625df6ddc7ac6adeb69f0b638a46b8
edwin yue <53625790+edwaddle@users.noreply.github.com>
2024-07-27 22:50:02 -0700

    completed first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, numOfTimes) {
+    if (numOfTimes <0){
+        return "ERROR";
+    }
+    let output = '';
+    for (let i = 0; i < numOfTimes; i++){
+        output += str;
+    }
+    return output;
 };
 
 // Do not edit below this line
