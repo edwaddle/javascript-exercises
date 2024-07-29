e6dc4b1b44ce9e54ca72100018b51e8fec41e296
edwin yue <53625790+edwaddle@users.noreply.github.com>
2024-07-28 22:17:06 -0700

    finished problems 7 and 8

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeInFarenHeit) {
+  return Math.round((degreeInFarenHeit - 32)*(5.0/9)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeInCelsius) {
+  return Math.round((degreeInCelsius*(9.0/5) + 32)*10)/10;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  for (num of array){
+    total += num;
+  }
+  return total;
+  
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (num of array){
+    total *= num;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let total = num1;
+  while (num2 > 1){
+    total *= num1;
+    num2--;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  if (num === 1 || num === 0){
+    return total;
+  }
+  else{
+    for (let i = 1; i <= num; i++){
+      total *= i;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
