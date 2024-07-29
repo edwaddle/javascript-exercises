dc2fed354d6b6d92364b2595324965ef6eabb36e
edwin yue <53625790+edwaddle@users.noreply.github.com>
2024-07-28 22:54:04 -0700

    finished problem 9 and 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    //3rd line gets rid of special characters, 4th line gets rid of spaces
+    str = str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+    str = str.replace(/\s+/g, '');
+    str = str.toLowerCase();
+    str2 = '';
+    for (let i = 1; i <= str.length; i++){
+        str2 += (str.charAt(str.length-i));
+    }
+    console.log(str + " " + str2);
+    return str === str2;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0){ return "OOPS"}
+    num = parseInt(num);
+    let output = 0;
+    let prevNum = 1;
+    let prevPrevNum = 1;
+    if (num === 1 || num === 2){ //base case of 1 and 2, where the answers will be 1
+        return 1;
+    }
+    for (let i = num; i > 2; i--){
+        output = prevNum + prevPrevNum;
+        prevNum = prevPrevNum;
+        prevPrevNum = output; 
+    }
+    return output;
 };
 
 // Do not edit below this line
