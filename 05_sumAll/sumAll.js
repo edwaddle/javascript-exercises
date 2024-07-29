f6a2bc771c4ab1f12eeaf917a4c0c0ac8bbed034
edwin yue <53625790+edwaddle@users.noreply.github.com>
2024-07-28 22:00:40 -0700

    Solved questions 5 and 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 <0 || num2 < 0 || (num1 +1) != (parseInt(num1+1)) || 
+    (num2+1) != (parseInt(num2)+1)){ //added 1 in order to check if its a string
+        //a string will add number to the end of the string, whereas an in will just add the number
+        return "ERROR";
+    }
+    else if (num1 > num2){
+        let tempNum = num1;
+        num1 = num2;
+        num2 = tempNum;
+    }
+    let total = 0;
+    for (let i = num1; i <= num2; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
